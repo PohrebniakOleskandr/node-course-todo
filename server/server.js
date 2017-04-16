d817ececba50a30380016058122f6818e285b58b
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-04-16 15:24:27 +0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
@@ -65,6 +66,37 @@ app.get('/todos/:id',(req,res)=>{
     })
     .catch((e)=>res.status(400).send('Error while fetching todo from database'));
 });
+
+
+app.patch('/todos/:id', (req,res) => {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectId.isValid(id))
+    {
+      return res.status(404).send("The todo ID is invalid");
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    }
+    else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send(todo);
+    }).catch((e) => {
+      res.status(400).send();
+    });
+});
+
+
 
 app.listen(port, ()=> {
   console.log(`Started on port ${port}`);
